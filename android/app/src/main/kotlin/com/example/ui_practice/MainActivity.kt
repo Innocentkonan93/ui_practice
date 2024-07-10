7802741638f1a7fdee5810b79642f8db33d63b78
Innocenkonan93 <konankoffiinnocent@gmail.com>
2024-07-10 15:06:25 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/ui_practice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ui_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  final enterText = "".obs;
+  final enteredPassword = "".obs;
+  final showCursor = true.obs;
+  final count = 0.obs;
+  final isIconOnRight = true.obs;
+  final isValidated = false.obs;
+}
